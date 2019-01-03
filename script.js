3014d92fca24d9912b3e19e7e164bf72bb43cae6
Mo Rob <31892724+morob1@users.noreply.github.com>
2019-01-03 18:36:53 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+$(function() {
+ 
+  $('.contact-form-jqscroll').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000); 
+          
+        return false;
+      }
+    }
+  });
+});
+
+$(function() {
+ 
+  $('.services-jqscroll').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000); 
+          
+        return false;
+      }
+    }
+  });
+});
+
+$(function() {
+ 
+  $('.contact-form-jqscroll').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000); 
+          
+        return false;
+      }
+    }
+  });
+});
